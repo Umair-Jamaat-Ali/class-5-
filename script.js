72d26cf9e0e045e5e6d00ff3feb65ec099146cce
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-04-01 23:21:44 +0500

    even odd program

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 // console.log(document)
 
-var heading = document.getElementById("heading")
+// var heading = document.getElementById("heading")
+
+
+// function setHeading() {
+//     console.log("setHeading call")
+
+//     heading.innerHTML = "Number is even or odd"
+// }
+
+
+function evenOdd() {
+    var input = document.getElementById("number")
+    var pera = document.getElementById("pera")
+
+    if(input.value % 2 == 0){
+        pera.innerHTML = "number is even"
+    }
+
+    else{
+        pera.innerHTML = "number is odd"
+    }
+}
